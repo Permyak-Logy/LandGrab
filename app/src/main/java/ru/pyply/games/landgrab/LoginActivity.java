f4963c9f018c6f13af529ee6f6edb48a0b512775
Permyak Logy <maxsika2004@mail.ru>
2021-01-14 15:40:32 +0400

    Изменены надписи об успехе и ошибке при входе, цвета этих надписей, а также добавленна очистка полей при ошибке
    (Ну и ещё удалены некоторые неиспользуемые ресурсы)

--- app/src/main/java/ru/pyply/games/landgrab/LoginActivity.java
+++ app/src/main/java/ru/pyply/games/landgrab/LoginActivity.java
@@ -2,14 +2,17 @@ package ru.pyply.games.landgrab;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+
 public class LoginActivity extends Activity {
-    public static String email = "max@mail.ru";
-    public static String password = "qwerty123";
+    public static final String email = "max@mail.ru";
+    public static final String password = "qwerty123";
 
     public static final byte REQ_REGISTRATION_CODE = 0;
 
@@ -19,6 +22,7 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public void onClickBtnLogin(View view) {
         TextView text_view_msg = (TextView) findViewById(R.id.textMessagesLogin);
         EditText text_edit_email = (EditText) findViewById(R.id.editTextEmailLogin);
@@ -30,10 +34,20 @@ public class LoginActivity extends Activity {
         if (input_email.equals(email) && password.equals(input_password)) {
             // При успешном входе мы говорим об успехе
             text_view_msg.setText(R.string.success_login_msg);
+            text_view_msg.setTextColor(getColor(R.color.green));
         } else {
+            text_view_msg.setText(R.string.fail_login_msg);
+            text_view_msg.setTextColor(getColor(R.color.red));
+
+            text_edit_email.setText("");
+            text_edit_password.setText("");
+
+            /* Временно заглушим этот вереход на регистрацию
+
             // Иначе отправляем на регистрацию
             Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
             startActivityForResult(i, REQ_REGISTRATION_CODE);
+            */
         }
     }
 
