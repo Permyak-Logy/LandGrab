935e69def4e421321942cdbab18ed6791261afbb
Permyak Logy <maxsika2004@mail.ru>
2021-03-17 19:50:09 +0400

    Добавлен предварительный функционал и дизайн для активности

--- app/src/main/java/ru/pyply/games/landgrab/PlayOfflineActivity.java
+++ app/src/main/java/ru/pyply/games/landgrab/PlayOfflineActivity.java
@@ -2,7 +2,9 @@ package ru.pyply.games.landgrab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PlayOfflineActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class PlayOfflineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_offline);
+    }
+
+    public void startOfflineGame(View view) {
+        Intent i = new Intent(PlayOfflineActivity.this, GameActivity.class);
+        startActivity(i);
+    }
+
+    public void showSettingsOffline(View view) {
+        Intent i = new Intent(PlayOfflineActivity.this, SettingsGame.class);
+        startActivityForResult(i, 0);
     }
 }
