1e05f9f553edbbc96cedf1996de67574aa8cb363
Permyak Logy <maxsika2004@mail.ru>
2021-03-17 18:25:12 +0400

    Добавленны активности Обновлён фрагмент game_info и другие

---
+++ app/src/main/java/ru/pyply/games/landgrab/PlayOfflineActivity.java
@@ -0,0 +1,14 @@
+package ru.pyply.games.landgrab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayOfflineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_offline);
+    }
+}
---
+++ app/src/main/java/ru/pyply/games/landgrab/SettingsGame.java
@@ -0,0 +1,14 @@
+package ru.pyply.games.landgrab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_game);
+    }
+}
