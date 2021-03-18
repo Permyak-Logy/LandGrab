432772153374706642852e7b946c460c2cd0d2f3
Permyak Logy <maxsika2004@mail.ru>
2021-03-18 16:47:42 +0400

    Добавлен предварительный дизайн и функционал для активности

--- app/src/main/java/ru/pyply/games/points/AccountActivity.java
+++ app/src/main/java/ru/pyply/games/points/AccountActivity.java
@@ -3,6 +3,7 @@ package ru.pyply.games.points;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+    }
+
+    public void saveAndExit(View view) {
+        finish();
     }
 }
