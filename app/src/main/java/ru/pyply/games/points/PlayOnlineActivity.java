2fb7a3424410bfead8d7eec866c07bcac5581ad9
Permyak Logy <maxsika2004@mail.ru>
2021-03-18 15:10:28 +0400

    Добавлен предварительный дизайн и функционал для активности

--- app/src/main/java/ru/pyply/games/points/PlayOnlineActivity.java
+++ app/src/main/java/ru/pyply/games/points/PlayOnlineActivity.java
@@ -2,7 +2,9 @@ package ru.pyply.games.points;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PlayOnlineActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class PlayOnlineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_online);
+    }
+
+    public void createLobby(View view) {
+        Intent i = new Intent(PlayOnlineActivity.this, LobbyActivity.class);
+        startActivity(i);
     }
 }
